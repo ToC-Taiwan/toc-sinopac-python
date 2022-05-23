c45551686d6227d7460a28a518725125a80eea91
TimHsu@DevContainer <maochindada@gmail.com>
2022-05-23 09:54:45 +0800

    chore(proto): update compile proto script

--- scripts/compile-proto.sh
+++ scripts/compile-proto.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
+git clone git@gitlab.tocraw.com:root/toc-trade-protobuf.git
 python -m grpc_tools.protoc -I./protobuf --python_out=./src --mypy_out=./src --grpc_python_out=./src ./protobuf/*.proto
+rm -rf toc-trade-protobuf
