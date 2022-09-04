98bd19f874d586bfb67350bc9d119d1ce47c1c02
TimHsu@DevContainer <maochindada@gmail.com>
2022-09-04 22:04:12 +0800

    chore(cron): modify afternoon terminate time to 14:40, disable auto deployment

--- src/cron.py
+++ src/cron.py
@@ -8,7 +8,7 @@ import schedule
 def add_schedule_job():
     schedule.every().day.at("01:20").do(os._exit, 0)
     schedule.every().day.at("08:20").do(os._exit, 0)
-    schedule.every().day.at("15:20").do(os._exit, 0)
+    schedule.every().day.at("14:40").do(os._exit, 0)
     while True:
         schedule.run_pending()
         time.sleep(10)
