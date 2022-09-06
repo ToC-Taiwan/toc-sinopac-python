59f609f2c128cc1b841b49c7225d7dbc40e5b012
TimHsu@DevContainer <maochindada@gmail.com>
2022-09-07 02:24:40 +0800

    feat(cron): remove 1:20 terminate

--- src/cron.py
+++ src/cron.py
@@ -6,7 +6,6 @@ import schedule
 
 
 def add_schedule_job():
-    schedule.every().day.at("01:20").do(os._exit, 0)
     schedule.every().day.at("08:20").do(os._exit, 0)
     schedule.every().day.at("14:40").do(os._exit, 0)
     while True:
