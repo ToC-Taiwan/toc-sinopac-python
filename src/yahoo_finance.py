65ab73d820dec8c75b7bcbf23592a8c3a4826f50
TimHsu@DevContainer <maochindada@gmail.com>
2022-11-21 22:17:37 +0800

    ci(deployment): run specific image tag instead of latest, catch yahoo finance key error

--- src/yahoo_finance.py
+++ src/yahoo_finance.py
@@ -3,9 +3,15 @@ import yfinance as yf
 
 class Yahoo:
     def get_nasdaq(self):
-        t = yf.Ticker("^IXIC")
-        return [float(t.info["regularMarketPrice"]), float(t.info["previousClose"])]
+        try:
+            t = yf.Ticker("^IXIC")
+            return [float(t.info["regularMarketPrice"]), float(t.info["previousClose"])]
+        except KeyError:
+            return [0, 0]
 
     def get_nasdaq_future(self):
-        t = yf.Ticker("NQ=F")
-        return [float(t.info["regularMarketPrice"]), float(t.info["previousClose"])]
+        try:
+            t = yf.Ticker("NQ=F")
+            return [float(t.info["regularMarketPrice"]), float(t.info["previousClose"])]
+        except KeyError:
+            return [0, 0]
