e1ba930cc6cdfa03e4e4e42b4fce8a1ba1392078
TimHsu@DevContainer <maochindada@gmail.com>
2023-02-02 14:49:56 +0800

    fix(cron): fix redundant argument in shcedule terminate

--- src/cron.py
+++ src/cron.py
@@ -9,8 +9,8 @@ def schedule_exit():
 
 
 def add_schedule_job():
-    schedule.every().day.at("08:20").do(schedule_exit, 0)
-    schedule.every().day.at("14:40").do(schedule_exit, 0)
+    schedule.every().day.at("08:20").do(schedule_exit)
+    schedule.every().day.at("14:40").do(schedule_exit)
     while True:
         schedule.run_pending()
         time.sleep(10)
