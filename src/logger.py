7806b075a9e78064f821e9f1cf17ec393cf159c4
Tim Hsu@M2MAX-20230327 <maochindada@gmail.com>
2023-12-05 17:39:22 +0800

    refactor: refactor log formatting based on environment variables
    - Refactored the `RFC3339Formatter` class to handle log format initialization based on environment variables.
    - Integrated environment variable 'is_file' and 'is_json' into the `console_handler` and `file_handler` formatters.
    - Removed static assignment of `LOG_FORMAT` and moved the logic to the class `RFC3339Formatter`'s constructor.

--- src/logger.py
+++ src/logger.py
@@ -4,17 +4,23 @@ from logging import LogRecord
 
 from env import RequiredEnv
 
-LOG_FORMAT = str()
 env = RequiredEnv()
-
-if env.log_format == "json":
-    LOG_FORMAT = '{"time":"%(asctime)s","user":"%(name)s","level":"%(levelname)s","message":"%(message)s"}'
-else:
-    LOG_FORMAT = "%(levelname)s\x1b[0m[%(asctime)s] %(message)s"
 
 
 class RFC3339Formatter(logging.Formatter):
+    def __init__(self, is_file: bool = False, is_json: bool = False):
+        self.log_format = "%(levelname)s\x1b[0m[%(asctime)s] %(message)s"
+        self.is_file = is_file
+        if is_file:
+            self.log_format = "%(levelname)s[%(asctime)s] %(message)s"
+        if is_json:
+            self.log_format = '{"time":"%(asctime)s","user":"%(name)s","level":"%(levelname)s","message":"%(message)s"}'
+        super().__init__(self.log_format)
+
     def format(self, record: LogRecord) -> str:
+        if self.is_file:
+            return super().format(record)
+
         color_code = "\x1b[36m"
         if record.levelno == logging.DEBUG:
             color_code = "\x1b[37m"
@@ -33,12 +39,11 @@ class RFC3339Formatter(logging.Formatter):
         return local_time.isoformat(timespec="seconds")
 
 
-formatter = RFC3339Formatter()
 console_handler = logging.StreamHandler()
 file_handler = logging.FileHandler(f'logs/{datetime.now().strftime("%Y-%m-%d")}-toc-sinopac-python.log')
 
-console_handler.setFormatter(RFC3339Formatter(fmt=LOG_FORMAT))
-file_handler.setFormatter(RFC3339Formatter(fmt=LOG_FORMAT))
+console_handler.setFormatter(RFC3339Formatter(is_file=False, is_json=env.log_format == "json"))
+file_handler.setFormatter(RFC3339Formatter(is_file=True, is_json=env.log_format == "json"))
 
 logging.addLevelName(50, "CRIT")
 logging.addLevelName(40, "ERRO")
